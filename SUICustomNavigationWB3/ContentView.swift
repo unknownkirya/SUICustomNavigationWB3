190c442e57d43a295303f0fd4af34a0d3f084826
unknownkirya <unknownkirya@gmail.com>
2024-07-21 14:06:58 +0300

    Initial Commit

---
+++ SUICustomNavigationWB3/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SUICustomNavigationWB3
+//
+//  Created by Kirill on 21.07.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SUICustomNavigationWB3/SUICustomNavigationWB3App.swift
@@ -0,0 +1,17 @@
+//
+//  SUICustomNavigationWB3App.swift
+//  SUICustomNavigationWB3
+//
+//  Created by Kirill on 21.07.2024.
+//
+
+import SwiftUI
+
+@main
+struct SUICustomNavigationWB3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
